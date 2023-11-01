b2f8d3e841b9a9466bbfbf0829d72444bd779267
Akash1211222 <90515043+Akash1211222@users.noreply.github.com>
2023-11-01 23:42:39 +0530

    some update

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 import {fetchMovieAvailability,fetchMovieList} from "./api.js"
+const bookTicketBtn = document.querySelector('#book-ticket-btn')
+bookTicketBtn.addEventListener('click', (e) => handleBookTicket(e))
+let selectedSeats = []
+function htmlToElement(html) {
+    const template = document.createElement('template')
+    html = html.trim();
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
